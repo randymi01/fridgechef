bce79ae7793591e3749d7bde14b408df102f0963
Kevin Zhu <kevin691827@gmail.com>
2023-02-09 19:14:26 -0500

    Started working on a jac file with python integration

---
+++ calculator/calculator.py
@@ -0,0 +1,5 @@
+from jaseci.actions.live_actions import jaseci_action
+
+@jaseci_action(act_group=["timestamp"], allow_remote=True)
+def add(first_number: int, second_number: int):
+    return first_number + second_number
