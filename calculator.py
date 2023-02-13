e385e0bb184fdeae28dd3657cc8156bd6d23dcbd
Randy <randymi@umich.edu>
2023-02-13 14:33:28 -0500

    deleted calc

--- calculator.py
+++
@@ -1,5 +0,0 @@
-from jaseci.actions.live_actions import jaseci_action
-
-@jaseci_action(act_group=["timestamp"], allow_remote=True)
-def add(first_number: int, second_number: int):
-    return first_number + second_number
