3ccd82c350fae902b78ac919290f8cbfb2559c57
Randy <randymi@umich.edu>
2023-02-25 00:24:20 -0500

    sheesh

--- intent_classification/yn.py
+++ intent_classification/yn.py
@@ -3,6 +3,8 @@ from jaseci.actions.live_actions import jaseci_action
 
 classifier = pipeline('sentiment-analysis')
 
-@jaseci_action(act_group=["yn_intent"], allow_remote=True)
-def yn_intent(text: str):
-    return classifier(text)[0]['label']
+#TODO save model so don't have to download everytime at runtime
+
+@jaseci_action(act_group=["intent"], allow_remote=True)
+def yn(text: str):
+    return 'yes' if classifier(text)[0]['label'] == "POSITIVE" else 'no'
