0bd16acb3f3a1009539db399e81e06e75ea21adb
Randy <randymi@umich.edu>
2023-03-15 14:53:02 -0400

    added record keeping and more node types

--- intent_classification/yn.py
+++ intent_classification/yn.py
@@ -3,6 +3,5 @@ from transformers import pipeline
 classifier = pipeline('sentiment-analysis')
 
 def yn_intent(text: str):
-    print(classifier(text)[0]['label'])
     return classifier(text)[0]['label']
 
