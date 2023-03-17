de20a8346c7ff0b59764d8c269dcf22bc2ce0630
= <randymi@umich.edu>
2023-03-17 11:27:40 -0400

    update

--- intent_classification/yn.py
+++ intent_classification/yn.py
@@ -1,11 +1,9 @@
 from transformers import pipeline
 import os
 
-if not os.path.exists('models/yn'):
-    classifier = pipeline('sentiment-analysis',model="distilbert-base-uncased-finetuned-sst-2-english")
-    classifier.save_pretrained(save_directory='models/yn')
-else:
-    classifier = pipeline('sentiment-analysis',model = "models/yn", tokenizer = "models/yn")
+# you have to download the model
+classifier = pipeline('sentiment-analysis',model="distilbert-base-uncased-finetuned-sst-2-english")
+
 
 def yn_intent(text: str):
     return classifier(text)[0]['label']
