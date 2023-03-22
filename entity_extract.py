9f845f14e571096fd3cd6dc97f6b4879abaec038
gcohenbr <84094583+gcohenbr@users.noreply.github.com>
2023-03-21 20:26:40 -0400

    Entity extraction model complete

--- entity_extract.py
+++ entity_extract.py
@@ -1,9 +1,9 @@
 import spacy
 from spellchecker import SpellChecker
 
-nlp = spacy.load('en_core_web_sm') # load English model
+nlp = spacy.load('food_ner_model') # load English model
 
-user_input = "blah blah blah (connection to be implemented)"
+user_input = "I have chicken, ketchup, a lot of butter, salt, and some lemon wedges. What can I make with these?" # DEMO - it works!
 
 words = user_input.split()
 
@@ -11,15 +11,17 @@ spell_check = SpellChecker()
 
 # Correct typos
 for i, word in enumerate(words):
+    idx = len(word) - 1
+    punc = ''
+    if word[idx] == ',' or word[idx] == '.' or word[idx] == '?' or word[idx] == '!':
+        punc = word[idx]
     if not spell_check.unknown([word]):
-        words[i] = word
+        words[i] = word + punc
     else:
-        words[i] = spell_check.correction(word)
-
+        words[i] = spell_check.correction(word) + punc
 user_input = ' '.join(words) # spellchecked user input
 
 processed = nlp(user_input) # process user input
-
 # Extract ingredients
 ingredients = []
 for entity in processed.ents:
@@ -29,3 +31,5 @@ for entity in processed.ents:
 ingredients = list(set(ingredients)) # remove duplicate ingredients
 
 # TODO: send ingredients list to where it needs to be.
+
+print(ingredients)
