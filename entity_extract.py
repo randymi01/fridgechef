04bb462f337e89f4589f559143b714a82f2a8ac5
gcohenbr <84094583+gcohenbr@users.noreply.github.com>
2023-03-21 20:45:27 -0400

    Update entity_extract.py

--- entity_extract.py
+++ entity_extract.py
@@ -1,35 +1,36 @@
 import spacy
 from spellchecker import SpellChecker
 
-nlp = spacy.load('food_ner_model') # load English model
+def food_extract(user_input):
+    nlp = spacy.load('food_ner_model') # load English model
 
-user_input = "I have chicken, ketchup, a lot of butter, salt, and some lemon wedges. What can I make with these?" # DEMO - it works!
+    words = user_input.split()
 
-words = user_input.split()
+    spell_check = SpellChecker()
 
-spell_check = SpellChecker()
+    # Correct typos
+    for i, word in enumerate(words):
+        idx = len(word) - 1
+        punc = ''
+        if word[idx] == ',' or word[idx] == '.' or word[idx] == '?' or word[idx] == '!':
+            punc = word[idx]
+        if not spell_check.unknown([word]):
+            words[i] = word + punc
+        else:
+            words[i] = spell_check.correction(word) + punc
+    user_input = ' '.join(words) # spellchecked user input
 
-# Correct typos
-for i, word in enumerate(words):
-    idx = len(word) - 1
-    punc = ''
-    if word[idx] == ',' or word[idx] == '.' or word[idx] == '?' or word[idx] == '!':
-        punc = word[idx]
-    if not spell_check.unknown([word]):
-        words[i] = word + punc
-    else:
-        words[i] = spell_check.correction(word) + punc
-user_input = ' '.join(words) # spellchecked user input
+    processed = nlp(user_input) # process user input
+    # Extract ingredients
+    ingredients = []
+    for entity in processed.ents:
+        if entity.label_ == 'FOOD':
+            ingredients.append(entity.text.lower())
 
-processed = nlp(user_input) # process user input
-# Extract ingredients
-ingredients = []
-for entity in processed.ents:
-    if entity.label_ == 'FOOD':
-        ingredients.append(entity.text.lower())
+    ingredients = list(set(ingredients)) # remove duplicate ingredients
+    return ingredients
 
-ingredients = list(set(ingredients)) # remove duplicate ingredients
-
-# TODO: send ingredients list to where it needs to be.
-
+# DEMO
+ui = "I have chicken, kethhup, a lot of butter, salt, and some lemon wedges, and butter. What can I make with these?" # example
+ingredients = food_extract(ui)
 print(ingredients)
