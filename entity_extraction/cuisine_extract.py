ddcad57a9a8c80b43bb5b6db872d826a5f1b42d1
Kevin Zhu <kevin691827@gmail.com>
2023-03-24 17:31:29 -0400

    cuisine_extraction

---
+++ entity_extraction/cuisine_extract.py
@@ -0,0 +1,28 @@
+import spacy
+
+# Load the pre-trained spaCy model
+nlp = spacy.load("en_core_web_sm")
+
+def cuisine_extract(user_input):
+    # Define the cuisine names
+    cuisines = ["african", "american", "aritish", "cajun", "caribbean", "chinese", 
+                "eastern european", "european", "french", "german", "greek", "indian", 
+                "irish", "italian", "japanese", "jewish", "korean", "latin american", 
+                "mediterranean", "mexican", "middle eastern", "nordic", "southern", "spanish", "thai", "vietnamese"]
+
+    # Process the text using the spaCy model
+    doc = nlp(user_input)
+
+    # Extract the cuisines from the text
+    cuisine_matches = []
+    for token in doc:
+        if token.text.lower() in cuisines:
+            cuisine_matches.append(token.text)
+
+    # Print the extracted cuisines
+    return cuisine_matches
+
+# DEMO
+ui = "I prefer to eat korean food"
+cuisines = cuisine_extract(ui)
+print(cuisines)
